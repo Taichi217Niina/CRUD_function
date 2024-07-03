cc3b41e7cabf3cae2c37b1b219c49f500cd596f4
tayboy217 <taichi217niina@gmail.com>
2024-07-03 17:19:31 +0900

    Add Mapper及びDB接続処理追記

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+/**
+ * Userエンティティクラス.
+ * 
+ * @author niinataichi
+ *
+ */
+@Data
+public class User {
+	
+	private Long id;
+    
+	private String name;
+    
+	private String email;	
+	
+
+}
---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.demo.entity.User;
+
+@Mapper
+public interface UserMapper {
+	
+    User findById(Long id);
+
+    List<User> findAll();
+
+    void insert(User user);
+
+    void updateInfo(User user);
+
+    void deleteById(Long id);
+
+}
